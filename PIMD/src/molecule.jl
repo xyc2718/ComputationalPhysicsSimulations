3e3c8a82819d0fa633b9db946e9c5b173aea1664
xyc <22307110070@m.fudan.edu.cn>
2024-12-17 19:31:05 +0800

    add module molecule

---
+++ PIMD/src/molecule.jl
@@ -0,0 +1,11 @@
+module MoleculeUtils
+
+"""
+Map atoms and molecule to the pos of atoms in cell
+"""
+function mapCell2Molecue(cell::UnitCell, molecule::Molecule,atoms::Vector{Atom})
+
+    
+end
+
+end
